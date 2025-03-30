3123a48bd3e16f4c9e13e71ae6de3ea9ff7551ed
Anita <anitatorabi.aut@gmail.com>
2025-03-30 16:55:08 +0330

    add Database class to manage entities

--- src/db/Database.java
+++ src/db/Database.java
@@ -1,4 +1,46 @@
 package db;
+import db.*;
+import db.exception.EntityNotFoundException;
+
+import java.util.ArrayList;
 
 public class Database {
+    private static final ArrayList<Entity> entities = new ArrayList<>();
+    private static int nextId = 1;
+
+    private Database() {}
+
+    public static void add(Entity e) {
+        e.id = nextId++;
+        entities.add(e);
+    }
+
+    public static Entity get(int id) {
+        for (Entity entity : entities) {
+            if (entity.id == id) {
+                return entity;
+            }
+        }
+        throw new EntityNotFoundException(id);
+    }
+
+    public static void delete(int id) {
+        for (Entity entity : entities) {
+            if (entity.id == id) {
+                entities.remove(entity);
+                return;
+            }
+        }
+        throw new EntityNotFoundException(id);
+    }
+
+    public static void update(Entity e) {
+        for (int i = 0; i < entities.size(); i++) {
+            if (entities.get(i).id == e.id) {
+                entities.set(i, e);
+                return;
+            }
+        }
+        throw new EntityNotFoundException(e.id);
+    }
 }
