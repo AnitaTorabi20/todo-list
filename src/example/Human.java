9cf253472a27f648bac9f4868f49f73e81cbe9df
Anita <anitatorabi.aut@gmail.com>
2025-03-31 01:57:00 +0330

    add 'age' field to Human class

--- src/example/Human.java
+++ src/example/Human.java
@@ -3,13 +3,15 @@ import db.*;
 
 public class Human extends Entity {
     public String name;
+    public int age;
 
-    public Human(String name) {
+    public Human(String name, int age) {
         this.name = name;
+        this.age = age;
     }
     @Override
     public Human copy() {
-        Human copyHuman = new Human(name);
+        Human copyHuman = new Human(name,age);
         copyHuman.id = id;
 
         return copyHuman;
