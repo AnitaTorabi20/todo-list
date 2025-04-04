a8e7cbb2b042651e4183661bfad10beb72821d7c
Anita <anitatorabi.aut@gmail.com>
2025-04-05 01:28:42 +0330

    implement Step class

--- src/todo/entity/Step.java
+++ src/todo/entity/Step.java
@@ -1,4 +1,57 @@
 package todo.entity;
 
-public class Step {
+import db.Entity;
+import java.util.Date;
+
+public class Step extends Entity {
+    public enum Status {
+        NotStarted, Completed
+    }
+
+    private String title;
+    private Status status;
+    private int taskRef;
+    public static final int STEP_ENTITY_CODE = 18;
+
+    public Step(String title, int taskRef) {
+        this.title = title;
+        this.status = Step.Status.NotStarted;
+    }
+
+    @Override
+    public Step copy() {
+        Step copy = new Step(this.title, this.taskRef);
+        copy.status = this.status;
+        return copy;
+    }
+
+    @Override
+    public int getEntityCode() {
+        return STEP_ENTITY_CODE;
+
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public int getTaskRef(int taskRef) {
+        return taskRef;
+    }
+
+    public void setTaskRef(int taskRef) {
+        this.taskRef = taskRef;
+    }
 }
