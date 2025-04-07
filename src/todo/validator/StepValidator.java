c7097e73a42a5dfea38896b927b0c4d709aa4035
Anita <anitatorabi.aut@gmail.com>
2025-04-07 14:30:23 +0330

    fix StepValidator class problem

--- src/todo/validator/StepValidator.java
+++ src/todo/validator/StepValidator.java
@@ -1,4 +1,23 @@
 package todo.validator;
 
-public class StepValidator {
+import db.*;
+import db.exception.InvalidEntityException;
+import todo.entity.Step;
+
+public class StepValidator implements Validator {
+
+    @Override
+    public void validate(Entity entity) throws IllegalArgumentException, InvalidEntityException {
+        if (!(entity instanceof Step)) {
+            throw new IllegalArgumentException("Invalid entity type. Expected Step.");
+        }
+
+        if (((Step) entity).getTitle() == null || ((Step) entity).getTitle().trim().isEmpty()) {
+            throw new IllegalArgumentException("Step title cannot be null or empty");
+        }
+
+        if(Database.get(((Step) entity).getTaskRef()) == null)
+            throw new InvalidEntityException("Cannot save");
+
+    }
 }
