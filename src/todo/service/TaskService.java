031970dab5b5d0285f1489837d8cdc66c53a2942
Anita <anitatorabi.aut@gmail.com>
2025-04-07 14:32:29 +0330

    implement TaskSrvice class

--- src/todo/service/TaskService.java
+++ src/todo/service/TaskService.java
@@ -1,4 +1,56 @@
 package todo.service;
 
+import db.*;
+import db.exception.*;
+import todo.entity.*;
+import todo.service.*;
+import todo.validator.*;
+
 public class TaskService {
+    public static void setAsCompleted(int taskId) {
+        try {
+
+            Task task = (Task) Database.get(taskId);
+            task.setStatus(Task.Status.Completed);
+            Database.update(task);
+
+        } catch (EntityNotFoundException e) {
+            System.out.println("Task not found with id:" + taskId);
+        } catch (InvalidEntityException e) {
+            System.out.println("Task update failed" + e.getMessage());
+        }
+
+    }
+
+    public static void setAsInProgress(int taskId) {
+        try {
+
+            Task task = (Task) Database.get(taskId);
+            task.setStatus(Task.Status.InProgress);
+            Database.update(task);
+
+        } catch (EntityNotFoundException e) {
+            System.out.println("Task not found with id:" + taskId);
+        } catch (InvalidEntityException e) {
+            System.out.println("Task update failed" + e.getMessage());
+        }
+
+    }
+
+    public static void setAsNotStarted(int taskId) {
+        try {
+
+            Task task = (Task) Database.get(taskId);
+            task.setStatus(Task.Status.NotStarted);
+            Database.update(task);
+
+        } catch (EntityNotFoundException e) {
+            System.out.println("Task not found with id:" + taskId);
+        } catch (InvalidEntityException e) {
+            System.out.println("Task update failed" + e.getMessage());
+        }
+
+    }
+
+
 }
